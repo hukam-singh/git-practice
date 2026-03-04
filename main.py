57d42b2f40ec59794ac3fe1c21f93a4690ccdca3
hukam-singh <hukam1234ds@gmail.com>
2026-03-04 13:14:08 +0530

    add comment for name feature

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ def print_hi(name):
     print(f"Hi, {name}")
 
 if __name__ == "__main__":
+    # name input
     name = input("Enter your name : ")
     age = int(input("Enter your age"))
     print_hi(name, age)

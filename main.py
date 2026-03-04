caf68e0c10444a436aa8e0e7dda01710a64fc569
hukam-singh <hukam1234ds@gmail.com>
2026-03-04 13:16:14 +0530

    added feature 1

--- main.py
+++ main.py
@@ -4,4 +4,5 @@ def print_hi(name):
 if __name__ == "__main__":
     name = input("Enter your name : ")
     age = int(input("Enter your age"))
+    # added feature for feature-1 branch
     print_hi(name, age)
